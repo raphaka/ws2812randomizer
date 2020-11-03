0ccd35aa9a97bb8ccdec08dbe1c4e21c0f59c101
Raphael Kaemper <raphi.kaemper@web.de>
2020-11-03 19:57:48 +0100

    Ich hab keine Ahnung mehr was ich in diesem Commit veraendert habe

--- strip.py
+++ strip.py
@@ -1,8 +1,8 @@
-#from rpi_ws281x import *
+from rpi_ws281x import *
 import random
 
 # LED strip configuration:
-LED_COUNT      = 30
+LED_COUNT      = 60
 LED_PIN        = 18
 LED_FREQ_HZ    = 800000
 LED_DMA        = 10
@@ -14,24 +14,41 @@ LED_CHANNEL    = 0
 #strip.begin()
 
 iterations = 3
-shelf_start = [0,10,20]  #erste Led im Fach
-shelf_width = 10
+flash_period = 0.1
+shelf_start = [0,20,40]  #erste Led im Fach
+shelf_width = 20
+
+#COLORS
+roll_color_foreground = Color(255, 255, 255)
+roll_color_background = Color(0, 0, 0)
+choice_color_foreground = Color(0, 255, 0)
+choice_color_background = Color(0, 0, 0)
 
 def randomize():
+    #set all leds to choice background color
+    # for i in range(0,LED_COUNT):
+        # strip.setPixelColor(i, choice_color_foreground)
+    #choose random shelf
     hat = random.randint(0,len(shelf_start)-1)
     led_start = shelf_start[hat]
     led_end = led_start + shelf_width
     for i in range(led_start,led_end):
         print("pixel " + str(i))
-        #strip.setPixelColor(i, color)
+        # strip.setPixelColor(i, choice_color_foreground)
 
 def roll():
     for g in range(0,iterations):
+        #flash each shelf in foregground color for short time
         for h in range(0,len(shelf_start)):
             led_start = shelf_start[h]
             led_end = led_start + shelf_width
             for i in range(led_start,led_end):
                 print("pixel " + str(i))
+                # strip.setPixelColor(i, roll_color_foreground)
+            # time.sleep(flash_period)
+            # for i in range(led_start,led_end):
+            #     print("pixel " + str(i))
+            #     strip.setPixelColor(i, roll_color_background)
             print(" ")
 
 if __name__ == '__main__':
